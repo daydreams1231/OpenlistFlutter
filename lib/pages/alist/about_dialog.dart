94e27814237261df834e0a6e58b997d01f81dd20
daydreams <68049920+daydreams1231@users.noreply.github.com>
2025-06-27 19:10:59 +0800

    Update about_dialog.dart

--- lib/pages/alist/about_dialog.dart
+++ lib/pages/alist/about_dialog.dart
@@ -41,7 +41,7 @@ class _AppAboutDialogState extends State<AppAboutDialog> {
   @override
   Widget build(BuildContext context) {
     final alistUrl =
-        "https://github.com/alist-org/alist/releases/tag/$_alistVersion";
+        "https://github.com/OpenListTeam/OpenList/releases/tag/$_alistVersion";
     final appUrl =
         "https://github.com/jing332/AListFlutter/releases/tag/$_version";
     return AboutDialog(
